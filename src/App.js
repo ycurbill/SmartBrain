9d046ccd7d2451b2ac628b318cb68ce04cd18d8a
ycurbill <108531077+ycurbill@users.noreply.github.com>
2022-12-30 13:17:59 +0100

    front end is done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,128 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import { Component } from "react";
+import ParticlesBg from 'particles-bg'
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-      <Logo/>
-      {/*
-      <ImageLinkForm/>
-      <FaceRecognition/>
-      */}
-    </div>
-  );
+const USER_ID = 'yvan888';
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = '6aaf797f4da0494cbc48e131649e5fc8';
+const APP_ID = 'my-first-application';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';  
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = image.width;
+    const height = image.height;
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    let IMAGE_URL = this.state.input;
+
+    let raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+
+    this.setState({imageUrl: this.state.input});
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          this.displayFaceBox(this.calculateFaceLocation(result))
+        })
+        .catch(error => console.log('error', error));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false});
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true});
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" bg={true} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home' 
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition 
+                box={box} 
+                imageUrl={imageUrl}
+              />
+            </div> 
+          : (
+            route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange}/> 
+            : <Register onRouteChange={this.onRouteChange}/> 
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
 import './Logo.css';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className='tilt br2 shadow-2 w4'>
-                    <h3>React Parallax Tilt 👀</h3>
+            <Tilt className='tilt br2 shadow-2 w4 h4'>
+                <div className='pa3'>
+                    <img src={brain} alt='brain'/>
+                </div>
             </Tilt>
         </div>
     );
